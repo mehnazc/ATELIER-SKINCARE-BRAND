11d2d2e07d45842dc8d3f521dc9e9c4919b83372
Mehnaz Chowdhury <25046193-MehnazChowdhur1@users.noreply.replit.com>
2024-11-17 14:08:12 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,45 @@
+var i = 0;
+var txt = 'Beauty redefined, Naturally.';
+var speed = 50;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("demo").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+demo.style.paddingLeft="270px";
+demo.style.paddingTop = "33px";
+demo.style.fontSize="3em";
+demo.style.color="#074a5e"
+demo.style.position="center";
+demo.style.fontFamily="Questrial";
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
